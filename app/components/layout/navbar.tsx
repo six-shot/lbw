1bc0a60e7142b57307818a46994540eba6b08912
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2026-04-23 14:32:28 +0100

    fixes

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -6,18 +6,20 @@ import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <div className="h-[115px] bg-primary/20 px-20">
+    <div className="h-[115px] bg-primary/20 ">
       <div className="max-w-[1440px] mx-auto h-full px-10">
-        <div className="flex justify-between items-center h-full">
-          <Image src="/icon/lbw.svg" width={161} height={83} alt="lbw-logo" />
-          <nav className="flex gap-8 items-center">
-            {NAV_LINKS.map((link) => (
-              <Link key={link.label} href={link.href} className="text-mint">
-                {link.label}
-              </Link>
-            ))}
-            <Button size="lg">Get Tickets For Event</Button>
-          </nav>
+        <div className=" h-full">
+          <div className="flex justify-between items-center h-full">
+            <Image src="/icon/lbw.svg" width={161} height={83} alt="lbw-logo" />
+            <nav className="flex items-center gap-5 xl:gap-6 2xl:gap-8">
+              {NAV_LINKS.map((link) => (
+                <Link key={link.label} href={link.href} className="text-mint">
+                  {link.label}
+                </Link>
+              ))}
+              <Button size="lg">Get Tickets For Event</Button>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
