50a35b9e17ee81bc2a265f94d1523295db42445b
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2026-04-23 13:58:41 +0100

    fixes

--- app/sections/hero/index.tsx
+++ app/sections/hero/index.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="flex min-h-[860px] flex-col bg-[url('/hero.jpg')] bg-cover bg-center bg-no-repeat xl:min-h-[1026px]">
       <Navbar />
-      <div className="mx-auto flex w-full max-w-[1440px] flex-1 items-center justify-center px-6 pb-8 pt-6 xl:px-0 xl:pb-0">
+      <div className="mx-auto flex w-full max-w-[1440px] flex-1 items-center justify-center px-[56px] pb-8 pt-6 xl:px-0 xl:pb-0">
         <div className="flex w-full flex-col items-center justify-center rounded-[28px] border border-accent/10 bg-tertiary/3 px-6 py-12 backdrop-blur-[7px] lg:min-h-[560px] lg:px-10 xl:min-h-[698px] xl:rounded-[40px]">
           <h1 className="font-midnight text-center text-[72px] leading-[68px] lg:text-[108px] lg:leading-[95px] xl:text-[140px] xl:leading-[120px] 2xl:text-[160px] 2xl:leading-[131px]">
             Lagos Blockchain Week ‘26
